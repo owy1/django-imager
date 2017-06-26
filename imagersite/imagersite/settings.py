a7a9b1ed21514cf4c3f554dce7c6f2bde788c1a9
Ophelia Yin <owyowy1@gmail.com>
2017-06-26 00:40:30 -0700

    travis2

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'imager_profile',
-    'imagersite',
-    'registration'
+    'imagersite'
 ]
 
 MIDDLEWARE = [
