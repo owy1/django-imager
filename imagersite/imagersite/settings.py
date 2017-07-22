2a5f8696c7927adcea5ee5b0918a59c88cbbb71f
Ophelia Yin <owyowy1@gmail.com>
2017-07-21 18:10:24 -0700

    add aws EC2 instance in setting

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-50-112-10-183.us-west-2.compute.amazonaws.com']
 
 ACCOUNT_ACTIVATION_DAYS = 7
 
